a556bcc5758b4d669246b0c1dd5a1690a982e946
alioz <aliozkanmavis@gmail.com>
2023-07-01 17:24:50 +0300

    basic register and HomePage UI

---
+++ lib/const/app_text.dart
@@ -0,0 +1,7 @@
+class AppText {
+  static final email = "Email";
+  static final password = "Şifre";
+  static final login = "Giriş";
+  static final orSignIn = "Hesabım yok Kaydol";
+  static final signIn = "Kaydol";
+}
